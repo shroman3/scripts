a9211b70acae917ae992e2c025ffbcd87be74bad
shroman <shroman@lap476.cs.technion.ac.il>
2017-06-16 14:43:50 +0300

    adding encode experiment

--- Measurements/generate_random_file.py
+++ Measurements/generate_random_file.py
@@ -3,7 +3,7 @@ import sys
 
 MEGA=1048576
 
-def generate_random_file(output_filename, megas)
+def generate_random_file(output_filename, megas):
     with open(output_filename, 'wb') as fout:
         fout.write(os.urandom(MEGA*megas))
 
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print 'usage: generate.py output_file megabytes'
         sys.exit(2)
-    generate_random_file(sys.argv[1]), int(sys.argv[2]))
+    generate_random_file(sys.argv[1], int(sys.argv[2]))
--- Measurements/parallel_platform.py
+++ Measurements/parallel_platform.py
@@ -40,10 +40,10 @@ import xml.etree.ElementTree
 #         if output:
 #             sys.stdout.write(prefix + nextline)
 #             sys.stdout.flush()
-# 
+#  
 #     output = proc.communicate()[0]
 #     exitCode = proc.returncode
-# 
+#  
 #     if (exitCode == 0):
 #         return output
 #     else:
@@ -78,26 +78,31 @@ class ParallelPlatform:
         # define dirs
         self.scdir = "/home/shroman/sraid"
         self.scriptsdir = self.scdir + "/scripts"
-        self.resultsdir = self.scdir + "/results"
+        self.resultsdir = "/sraid/results"
 
         os.chdir(self.scdir)
         
         self.servers = {}
         # choose servers, must be ordered
-        if (len(sys.argv) != 2):
+        if (len(sys.argv) != 8):
             print "Please call the parallel platform in the following manner:"
-            print "./parallel_platform.py experiment_filename"
+            print "parallel_platform.py exp codec k r z random_name random_key"
             exit(0)
         # parse arguments:
+        self.parse_arguments()
         self.parse_servers()
-        self.parse_experiment(sys.argv[1])
 
-    def parse_experiment(self, filename):
-        os.chdir(self.scriptsdir)
-        with open(filename, "r") as f:
-            self.experiment = f.readline()
-
-        self.is_write = (self.experiment.startswith('w') or self.experiment.startswith('W'))
+    def parse_arguments(self):
+        self.exp = sys.argv[1]
+        self.codec = sys.argv[2]
+        self.k = sys.argv[3]
+        self.r = sys.argv[4]
+        self.z = sys.argv[5]
+#         self.n = self.k + self.r + self.z;
+        self.random_name = sys.argv[6]
+        self.random_key = sys.argv[7]
+        self.is_write = (self.exp.startswith('w') or self.exp.startswith('W') or self.exp.startswith('e') or self.exp.startswith('E'))
+        self.start_servers = not (self.exp.startswith('e') or self.exp.startswith('E'))
 
     def parse_servers(self):
         run_command("rm servers.txt")
@@ -113,6 +118,10 @@ class ParallelPlatform:
                 serversfile.write("%s\n" % serv)
 
     def experiment_get_ready(self):
+        print "removing old logs"
+        run_command("sudo rm /sraid/results/*.log")
+
+        
         run_permissions_command = "chmod +x /sraid/server/*.sh /sraid/server/*.py"
 
         # client clean up
@@ -137,8 +146,9 @@ class ParallelPlatform:
         parallelssh(run_permissions_command)
         
     def start_serevrs(self):
-        print "STARTING SERVERS"
-        parallelssh("sraid/scripts/startserver.sh")
+        if (self.start_servers):
+            print "STARTING SERVERS"
+            parallelssh("sraid/scripts/startserver.sh")
             
     def stop_serevrs(self):
         print "STOPING SERVERS"
@@ -153,45 +163,13 @@ class ParallelPlatform:
                 with open("0", mode='rb') as file:
                     fileContent = file.read()
             self.experiment_start = datetime.utcnow()
-            print self.experiment + " experiment started at:",
+            print self.exp + " " + self.codec + " experiment started at:",
             print self.experiment_start
-            check_output("java -jar client.jar " + self.experiment, shell=True)
+            
+            run_command("java -jar client.jar " + self.exp + " " + self.codec + " " + self.k + " " + self.r + " " + self.z + " " + self.random_name + " " + self.random_key)
         except Exception, e:
             print "FAILED"
             print e
-
- #     def parallel_unmount(self):
-#         print "remounting xfs partitions on all nodes"
-#         os.chdir(self.expdir)
-#         check_output("sudo ceph osd set noout", shell=True)
-#         sleep(20)
-# 
-#         try:  # should work for everyone except the down osd
-#             # give client access to servers list and copy parallel_unmount.sh
-#             # script
-#             check_output("parallel-scp -l shroman "
-#                     + "-h /homes/cephadmin/scripts/servers.txt "
-#                     + "/homes/cephadmin/scripts/servers.txt /tmp", shell=True)
-#             check_output("parallel-scp -l shroman "
-#                     + "-h /homes/cephadmin/scripts/servers.txt "
-#                     + "/homes/cephadmin/scripts/experiments/"
-#                     + "parallel_unmount.sh /tmp", shell=True)
-#             # execute parallel_unmount.sh script to unmount and remount
-#             # partitions on all servers at once
-#             check_output("parallel-ssh -h /homes/cephadmin/scripts/servers.txt"
-#                     + " -l shroman 'sudo chmod +x /tmp/parallel_unmount.sh; "
-#                     + "/tmp/parallel_unmount.sh'"
-#                     , shell=True)
-#             # remove added files
-#             check_output("parallel-ssh -h /homes/cephadmin/scripts/servers.txt"
-#                     + " -l shroman "
-#                     + "'sudo rm /tmp/servers.txt /tmp/parallel_unmount.sh'"
-#                     , shell=True)
-#         except Exception, e:
-#             print e
-#         sleep(20)
-#         check_output("sudo ceph osd unset noout", shell=True)
-#         self.poll_until_health_ok()
         
     def kill_measurements(self):
         # kill measurements
@@ -209,8 +187,9 @@ class ParallelPlatform:
         sleep(3)
 
     def execute(self):
-        print "executing: " + self.experiment
+        print "executing: " + self.exp + " " + self.codec
 
+        os.chdir(self.scriptsdir)
         self.stop_serevrs()
         self.experiment_get_ready()
         self.start_serevrs()
@@ -218,22 +197,14 @@ class ParallelPlatform:
         # start measurements...
         print "starting timer"
         timestart = datetime.utcnow()
-#         for serv in self.servers.keys():
-#             try:
-#                 print "running stat_parser.py on " + serv
-#                 stat_parser = Popen("sshpass -f p.txt ssh " + serv
-#                         + " 'nohup /sraid/server/stat_parser.py "
-#                         + timestart.isoformat() + " >/dev/null 2>&1 &'",
-#                         shell=True)
-#             except Exception, e:
-#                 print e
         run_stats_parser_command = "nohup /sraid/server/stat_parser.py "  + timestart.isoformat() + " >/dev/null 2>&1 &"
-        parallelssh(run_stats_parser_command)
         run_command(run_stats_parser_command)        
+        if (self.start_servers):
+            parallelssh(run_stats_parser_command)
+            print "sleeping for 3 seconds"
+            sleep(3)
         
         # conduct experiment...
-        print "sleeping for 5 seconds"
-        sleep(5)
         self.conduct_experiment()
 
         self.kill_measurements()
@@ -249,19 +220,19 @@ class ParallelPlatform:
         os.chdir(self.scriptsdir)
         print "copy stats to results folder"
         for serv in self.servers:
-            run_command("sshpass -f p.txt scp " + serv + ":/sraid/server/stats.txt ../results/" + serv + ".stat < p.txt")
-            run_command("sshpass -f p.txt scp " + serv + ":/sraid/server/logs/client_connection.log ../results/log1_" + serv + ".log < p.txt")
-            run_command("sshpass -f p.txt scp " + serv + ":/sraid2/server/logs/client_connection.log ../results/log2_" + serv + ".log < p.txt")
+            run_command("sshpass -f p.txt scp " + serv + ":/sraid/server/stats.txt /sraid/results/" + serv + ".stat < p.txt")
+            run_command("sshpass -f p.txt scp " + serv + ":/sraid/server/logs/client_connection.log /sraid/results/log1_" + serv + ".log < p.txt")
+            run_command("sshpass -f p.txt scp " + serv + ":/sraid2/server/logs/client_connection.log /sraid/results/log2_" + serv + ".log < p.txt")
             
-        run_command("cp /sraid/client/logs/* ../results/");
-        run_command("cp /sraid/server/stats.txt ../results/client.stat");
+        run_command("cp /sraid/client/logs/* /sraid/results/");
+        run_command("cp /sraid/server/stats.txt /sraid/results/client.stat");
 
     def parse_results(self):
         print "parsing results..."
         os.chdir(self.resultsdir)
-        output = open("results.txt", "a")
+        output = open("results.csv", "a")
         csvwriter = csv.writer(output)
-        csvwriter.writerow(['timestamp', 'inttime', 'experiment', 'machine', 'measurement', 'value'])
+        csvwriter.writerow(['timestamp', 'inttime', 'exp', 'codec', 'random', 'k', 'r', 'z', 'machine', 'measurement', 'value'])
         
         files = [f for f in os.listdir('.') if (os.path.isfile(f) and f.endswith(".stat"))]
         for file in files:
@@ -309,9 +280,9 @@ class ParallelPlatform:
 #         if int(self.osd_per_node) == 2 and measurement.startswith("io_p2"):
 #             osdnum += 1
         
-        # ['timestamp', 'inttime', 'experiment', 'machine', 'measurement', 'value'])
-        csvrow = [str(printed_time), str(int(printed_time)),
-                  self.experiment, server, measurement, str(value)];
+        # ['timestamp', 'inttime', 'exp', 'codec', 'k', 'r', 'z', 'machine', 'measurement', 'value'])
+        csvrow = [str(printed_time), str(int(printed_time)), self.exp, self.codec, self.random_name, 
+                  self.k, self.r, self.z, server, measurement, str(value)];
         
         csvwriter.writerow(csvrow)
 #         if measurement.startswith("net"):
@@ -325,9 +296,9 @@ class ParallelPlatform:
     def parse_logs(self):
         print "parsing logs..."
         os.chdir(self.resultsdir)
-        output = open("logs.txt", "a")
+        output = open("logs.csv", "a")
         csvwriter = csv.writer(output)
-        csvwriter.writerow(['experiment','log_type', 'time', 'tag', 'size'])
+        csvwriter.writerow(['exp', 'codec', 'random', 'k', 'r', 'z','log_type', 'time', 'tag', 'size'])
         
         files = [f for f in os.listdir('.') if (os.path.isfile(f) and f.endswith(".log"))]
         for file in files:
@@ -336,22 +307,18 @@ class ParallelPlatform:
             with open(file, "r") as f:
                 for line in f:
                     try:
-                        split = line.split(' - ')
-                        timestamp=split[0]
-                        split = re.compile("\]*[\s\w,-:]*\[+").split(split[1])
+                        split = re.compile("\]*[\s\w,-:]*\[+").split(line)
                         self.write_logs_row(csvwriter, log_type, split[2], split[3], split[4])
                     except Exception, e:
                         print e
+                        print line
                        
         output.close()
-#         print "removing logs and stats"
-#         run_command("rm *.log *.stat")
 
     def write_logs_row(self, csvwriter, log_type, time, tag, size):
         # ['experiment', 'log_type', 'time', 'tag', 'size'])
         size = size.split(']')[0]
-        csvrow = [self.experiment, log_type, time, tag] + size.split(',');
-        csvrow.append(object)
+        csvrow = [self.exp, self.codec, self.random_name, self.k, self.r, self.z, log_type, time, tag] + size.split(',');
         csvwriter.writerow(csvrow)
 
 if __name__ == "__main__":
